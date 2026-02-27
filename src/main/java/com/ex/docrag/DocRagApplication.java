48488b24caea7278b47103319671a48ec4cdc335
Fabio Floris <fabio.floris@eng.it>
2026-02-27 12:49:57 +0100

    Initial commit

---
+++ src/main/java/com/ex/docrag/DocRagApplication.java
@@ -0,0 +1,13 @@
+package com.ex.docrag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocRagApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocRagApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ex/docrag/DocRagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ex.docrag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocRagApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
